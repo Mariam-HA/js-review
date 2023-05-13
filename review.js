e5956d652d68a1c3ef85bb05e61600fdbf68cbf4
AMAL ALRASDAN <mariam.alrashdan97@gmail.com>
2023-05-13 23:30:51 +0300

    update

--- review.js
+++ review.js
@@ -53,3 +53,55 @@ function padelCourtcost(hour, courtType) {
   return padelCourtCost;
 }
 console.log(padelCourtcost(7, "indoors"));
+
+//functions and loops
+//part1
+
+temperature = [25, 26, 27, 28];
+function printer(elements) {
+  console.log(elements, "C");
+}
+temperature.forEach(printer);
+
+//part2
+const tempF = [99, 88, 77, 100];
+
+function toCelsius(numbers) {
+  return (numbers - 32) * (5 / 9);
+}
+const Celsius = tempF.map(toCelsius); // to create a new array from calling a function for every array element
+
+console.log(Celsius);
+
+//functions advanced
+
+// 1
+function convert(num) {
+  return num / 60;
+}
+console.log("The time in minutes =", convert(120));
+
+//2
+function isOdd(number) {
+  if (number % 2 == 1) {
+    return true;
+  }
+  return false;
+}
+console.log(isOdd(6));
+
+//3
+array = [1, 2, 3, 4, 5, 6, 7, 8];
+let addNumber = (num) => {
+  return array[0] + array[array.length - 1];
+};
+console.log("The sum of the first and last element = ", addNumber());
+
+//4
+function isArrayLengthEven(number) {
+  if (array.length % 2 == 0) {
+    return true;
+  }
+  return false;
+}
+console.log(isArrayLengthEven(array));
